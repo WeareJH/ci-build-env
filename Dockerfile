0c5f97276c0a7339e392480602e13a99fbe60e03
Diego Cabrejas <diego@wearejh.com>
2021-01-28 16:14:26 +0100

    Add support for composer2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:{{VERSION}}-cli-alpine
+FROM php:{{PHP_VERSION}}-cli-alpine
 MAINTAINER JH <hello@wearejh.com>
 
 RUN apk --update add \
@@ -68,7 +68,7 @@ RUN [ $(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") -ge 72 ] \
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 ENV PATH=/root/.composer/vendor/bin:$PATH
 
-RUN composer selfupdate --1
+RUN composer selfupdate --{{COMPOSER_VERSION}}
 RUN composer global require wearejh/m2-deploy-recipe:dev-master
 RUN composer global config repositories.ci-tool vcs git@github.com:WeareJH/ci-tool.git
 
--- build.sh
+++ build.sh
@@ -3,24 +3,37 @@
 HERE=$(dirname $0)
 
 PHPVER=${1}
+#composer version default to 1
+[[ ${#2} -lt 1 ]] && COMPOSERVER=1 || COMPOSERVER=${2}
 REPO=wearejh/ci-build-env
 SUPPORTED=(7.1 7.2 7.3 7.4)
+COMPOSER_SUPPORTED=(1 2)
 
-if [ ${#PHPVER} -lt 1 ]; then
-    echo "build.sh php-version"
+#Check all parameters are present
+if [ ${#PHPVER} -lt 1 ] || [ ${#COMPOSERVER} -lt 1 ]; then
+    echo "build.sh php-version composer-version"
     exit
 fi
-
+#Check PHP version supplied is supported
 if [[ ! " ${SUPPORTED[@]} " =~ " ${PHPVER} " ]]; then
     echo "PHP version ${PHPVER} not supported"
     exit
 fi
+#Check Composer version supplied is supported
+if [[ ! " ${COMPOSER_SUPPORTED[@]} " =~ " ${COMPOSERVER} " ]]; then
+    echo "Composer version ${COMPOSERVER} not supported"
+    exit
+fi
 
-# Pull current build for comparison before punblish
-docker pull ${REPO}:${PHPVER}
-docker tag ${REPO}:${PHPVER} ${REPO}:${PHPVER}-current
+# Pull current build for comparison before publish
+[[ $COMPOSERVER -ge 2 ]] && COMP_SUFFIX="-comp2" || COMP_SUFFIX=""
+IMAGE_URI=${REPO}:${PHPVER}${COMP_SUFFIX}
+
+docker pull ${IMAGE_URI}
+docker tag ${IMAGE_URI} ${IMAGE_URI}-current
 
 # PHP 7.x share a common Dockerfile
 # Set the required version in the Dockerfile
-sed -i -e "s/{{VERSION}}/${PHPVER}/g" Dockerfile
-docker build --no-cache -f Dockerfile -t ${REPO}:${PHPVER} ${HERE}
+sed -i -e "s/{{PHP_VERSION}}/${PHPVER}/g" Dockerfile
+sed -i -e "s/{{COMPOSER_VERSION}}/${COMPOSERVER}/g" Dockerfile
+docker build --no-cache -f Dockerfile -t ${IMAGE_URI} ${HERE}
