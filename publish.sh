1a6ff0b075390800bb5a794d299866712c5f6f10
Paweł Knap <me@pawelknap.com>
2026-03-12 13:56:55 +0100

    Handle first build in publish.sh when no previous image exists

--- publish.sh
+++ publish.sh
@@ -8,12 +8,16 @@ IMAGEVER=$(date '+%Y%m%d%H%M')
 # so that it is possible to target a specific build
 docker tag $IMAGE_URI $IMAGE_URI-$IMAGEVER
 
-# Compare image to current before push
-LAYER_DIFF=$(diff <(docker inspect $IMAGE_URI | jq '.[0].RootFS.Layers') <(docker inspect $IMAGE_URI-current | jq '.[0].RootFS.Layers'))
+# Compare image to current before push (skip on first build)
+if docker inspect $IMAGE_URI-current &> /dev/null; then
+    LAYER_DIFF=$(diff <(docker inspect $IMAGE_URI | jq '.[0].RootFS.Layers') <(docker inspect $IMAGE_URI-current | jq '.[0].RootFS.Layers'))
 
-if [ -z "${LAYER_DIFF}" ]; then
-    echo "Looks like this is exactly the same as the current build... skipping publish"
-    exit
+    if [ -z "${LAYER_DIFF}" ]; then
+        echo "Looks like this is exactly the same as the current build... skipping publish"
+        exit
+    fi
+else
+    echo "No previous build found, publishing first image"
 fi
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
